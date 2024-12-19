6391848e991f5816d33a043cbb392beb4c2c8846
vanajakunchala <vanajakshikunchala@gmail.com>
2024-12-19 12:32:28 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using WebApplication3.Models;
+
+
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index1(int? id, string name, double? price)
+        {
+            ViewData["Id"] = id;
+            ViewData["Name"] = name;
+            ViewData["Price"] = price;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
